278e3ab956d5ef9381e4eda5ad4fee38d53ccc00
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-03-03 17:06:18 +0530

    Add. Weather details fetching logic

---
+++ weather.js
@@ -0,0 +1,21 @@
+const apiKey = "7Ggx1BxQZWMsGZKBhypUrlLcvFbXAc6j";
+
+const getCityDetails = async (city) => {
+    const baseUrl = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const queryParam = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(baseUrl + queryParam);
+    const details = await response.json();
+
+    return details[0];
+};
+
+const getWeatherDetailsByCity = async (cityId) => {
+    const baseUrl = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(baseUrl + query);
+    const details = await response.json();
+
+    return details[0];
+};

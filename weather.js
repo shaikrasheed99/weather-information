4f828b8eb82b141cc5a1829c22b0cf2d46f3205f
shaikrasheed99 <shaikrasheed3014@gmail.com>
2023-03-03 17:26:52 +0530

    Refactor. Weather details fetching logic

--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@ const apiKey = "7Ggx1BxQZWMsGZKBhypUrlLcvFbXAc6j";
 
 const getCityDetails = async (city) => {
     const baseUrl = "http://dataservice.accuweather.com/locations/v1/cities/search";
-    const queryParam = `?apikey=${key}&q=${city}`;
+    const queryParam = `?apikey=${apiKey}&q=${city}`;
 
     const response = await fetch(baseUrl + queryParam);
     const details = await response.json();
@@ -12,7 +12,7 @@ const getCityDetails = async (city) => {
 
 const getWeatherDetailsByCity = async (cityId) => {
     const baseUrl = "http://dataservice.accuweather.com/currentconditions/v1/";
-    const query = `${id}?apikey=${key}`;
+    const query = `${cityId}?apikey=${apiKey}`;
 
     const response = await fetch(baseUrl + query);
     const details = await response.json();
